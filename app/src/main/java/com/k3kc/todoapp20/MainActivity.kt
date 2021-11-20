2bb6f0b479f5e5b631b81a083cf5bad306de49b1
x913 <x913x913@gmail.com>
2021-11-20 19:31:13 +0500

    add action bar

--- app/src/main/java/com/k3kc/todoapp20/MainActivity.kt
+++ app/src/main/java/com/k3kc/todoapp20/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.k3kc.todoapp20
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,18 +11,27 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
 import com.k3kc.todoapp20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+         setupActionBarWithNavController(navHostFragment.navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navHostFragment.navController.navigateUp() || super.onSupportNavigateUp()
+    }
 
 }
--- app/src/main/java/com/k3kc/todoapp20/fragments/list/ListFragment.kt
+++ app/src/main/java/com/k3kc/todoapp20/fragments/list/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.k3kc.todoapp20.R
+import com.k3kc.todoapp20.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
@@ -14,8 +16,11 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = FragmentListBinding.inflate(inflater, container, false)
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        return view.root
     }
 
 
