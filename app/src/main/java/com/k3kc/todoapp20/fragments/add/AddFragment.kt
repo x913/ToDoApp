bc2f1e0c6a91c46c5325aae4f9e457ce689b01ab
x913 <x913x913@gmail.com>
2021-11-20 17:41:45 +0500

    add fragment container view

--- app/src/main/java/com/k3kc/todoapp20/fragments/add/AddFragment.kt
+++ app/src/main/java/com/k3kc/todoapp20/fragments/add/AddFragment.kt
@@ -1,10 +1,11 @@
-package com.k3kc.todoapp20
+package com.k3kc.todoapp20.fragments.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.k3kc.todoapp20.R
 
 
 class AddFragment : Fragment() {
--- app/src/main/java/com/k3kc/todoapp20/fragments/list/ListFragment.kt
+++ app/src/main/java/com/k3kc/todoapp20/fragments/list/ListFragment.kt
@@ -1,10 +1,11 @@
-package com.k3kc.todoapp20
+package com.k3kc.todoapp20.fragments.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.k3kc.todoapp20.R
 
 
 class ListFragment : Fragment() {
