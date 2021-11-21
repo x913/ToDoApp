d4b5756e1619a7b657ef56e24b80658bd9800720
x913 <x913x913@gmail.com>
2021-11-22 00:19:06 +0500

    add fragment can insert data

--- app/src/main/java/com/k3kc/todoapp20/fragments/add/AddFragment.kt
+++ app/src/main/java/com/k3kc/todoapp20/fragments/add/AddFragment.kt
@@ -3,18 +3,22 @@ package com.k3kc.todoapp20.fragments.add
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.k3kc.todoapp20.R
 import com.k3kc.todoapp20.data.models.Priority
 import com.k3kc.todoapp20.data.models.ToDoData
 import com.k3kc.todoapp20.data.viewmodel.ToDoViewModel
 import com.k3kc.todoapp20.databinding.FragmentAddBinding
+import com.k3kc.todoapp20.fragments.SharedViewModel
 
 
 class AddFragment : Fragment() {
 
     private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
 
     private lateinit var _binding: FragmentAddBinding
     private val binding
@@ -31,6 +35,7 @@ class AddFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
+        binding.prioritiesSpinner.onItemSelectedListener = mSharedViewModel.listener
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -49,32 +54,25 @@ class AddFragment : Fragment() {
         val mPriority = binding.prioritiesSpinner.selectedItem.toString()
         val mDescription = binding.descriptionEt.text.toString()
 
-        val validation = verifyDataFromUser(mTitle, mDescription)
+        val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
         if(validation) {
             // insert data to database
             val newData = ToDoData(
                 0,
                 mTitle,
-                parsePriority(mPriority),
+                mSharedViewModel.parsePriority(mPriority),
                 mDescription
             )
             mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Successfully added", Toast.LENGTH_SHORT)
+                .show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
-    private fun verifyDataFromUser(title: String, description: String): Boolean {
-        return if(TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
-            false
-        } else !(title.isEmpty() || description.isEmpty())
-    }
 
-    private fun parsePriority(priority: String): Priority {
-        return when(priority) {
-            "High Priority" -> { Priority.HIGH }
-            "Low Priority" -> { Priority.LOW }
-            "Medium Priority" -> { Priority.MEDIUM }
-            else -> Priority.LOW
-        }
-    }
 
 }
