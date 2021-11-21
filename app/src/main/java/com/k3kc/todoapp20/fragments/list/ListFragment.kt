8abeffb2f2b221b6d654628946177b9090c1c6be
x913 <x913x913@gmail.com>
2021-11-22 00:20:36 +0500

    add recycler view with adapter into list fragment

--- app/src/main/java/com/k3kc/todoapp20/fragments/list/ListFragment.kt
+++ app/src/main/java/com/k3kc/todoapp20/fragments/list/ListFragment.kt
@@ -2,13 +2,20 @@ package com.k3kc.todoapp20.fragments.list
 
 import android.os.Bundle
 import android.view.*
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.k3kc.todoapp20.R
+import com.k3kc.todoapp20.data.viewmodel.ToDoViewModel
 import com.k3kc.todoapp20.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +28,14 @@ class ListFragment : Fragment() {
 
         view.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner) { data ->
+            adapter.setData(data)
         }
 
         setHasOptionsMenu(true)
