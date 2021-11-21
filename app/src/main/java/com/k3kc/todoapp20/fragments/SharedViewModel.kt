2b2553fe298e8dc74c976f48560a6798ec8a25c2
x913 <x913x913@gmail.com>
2021-11-22 00:19:44 +0500

    shared view model with recycler listener

---
+++ app/src/main/java/com/k3kc/todoapp20/fragments/SharedViewModel.kt
@@ -0,0 +1,43 @@
+package com.k3kc.todoapp20.fragments
+
+import android.app.Application
+import android.text.TextUtils
+import android.view.View
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import com.k3kc.todoapp20.R
+import com.k3kc.todoapp20.data.models.Priority
+
+class SharedViewModel(application: Application): AndroidViewModel(application) {
+
+    val listener: AdapterView.OnItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+             when(position) {
+                 0 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.red)) }
+                 1 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.yellow)) }
+                 2 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.green )) }
+             }
+        }
+
+        override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+    }
+
+    fun verifyDataFromUser(title: String, description: String): Boolean {
+        return if(TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
+            false
+        } else !(title.isEmpty() || description.isEmpty())
+    }
+
+    fun parsePriority(priority: String): Priority {
+        return when(priority) {
+            "High Priority" -> { Priority.HIGH }
+            "Low Priority" -> { Priority.LOW }
+            "Medium Priority" -> { Priority.MEDIUM }
+            else -> Priority.LOW
+        }
+    }
+
+}
