632f5405be2ea3d6b9038298e1849728cc87df16
x913 <x913x913@gmail.com>
2021-11-22 00:18:34 +0500

    add list adapter

---
+++ app/src/main/java/com/k3kc/todoapp20/fragments/list/ListAdapter.kt
@@ -0,0 +1,51 @@
+package com.k3kc.todoapp20.fragments.list
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.k3kc.todoapp20.R
+import com.k3kc.todoapp20.data.models.Priority
+import com.k3kc.todoapp20.data.models.ToDoData
+import com.k3kc.todoapp20.databinding.RowLayoutBinding
+import java.util.zip.Inflater
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    var dataList = emptyList<ToDoData>()
+
+    class MyViewHolder(private val binding: RowLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+        fun onBind(item: ToDoData) {
+            binding.titleTxt.text = item.title
+            binding.descriptionTxt.text = item.description
+
+            val priority = item.priority
+            when(priority) {
+                Priority.HIGH -> binding.priorityIndication.setCardBackgroundColor(ContextCompat.getColor(binding.root.context, R.color.red))
+                Priority.MEDIUM -> binding.priorityIndication.setCardBackgroundColor(ContextCompat.getColor(binding.root.context, R.color.yellow))
+                Priority.LOW -> binding.priorityIndication.setCardBackgroundColor(ContextCompat.getColor(binding.root.context, R.color.green))
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return MyViewHolder(RowLayoutBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.onBind(dataList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    fun setData(toDoData: List<ToDoData>) {
+        this.dataList = toDoData
+        notifyDataSetChanged()
+    }
+
+}
