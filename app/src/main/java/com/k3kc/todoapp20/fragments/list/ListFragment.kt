7b64b479d8b2f55de79ff19639b8220f92ac55ea
x913 <x913x913@gmail.com>
2021-12-06 00:01:14 +0500

    add search capabilities to list fragment

--- app/src/main/java/com/k3kc/todoapp20/fragments/list/ListFragment.kt
+++ app/src/main/java/com/k3kc/todoapp20/fragments/list/ListFragment.kt
@@ -3,63 +3,52 @@ package com.k3kc.todoapp20.fragments.list
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
-import androidx.core.view.isVisible
+import android.widget.GridLayout
+//import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.k3kc.todoapp20.R
+import com.k3kc.todoapp20.data.models.ToDoData
 import com.k3kc.todoapp20.data.viewmodel.ToDoViewModel
-import com.k3kc.todoapp20.databinding.FragmentAddBinding
 import com.k3kc.todoapp20.databinding.FragmentListBinding
 import com.k3kc.todoapp20.fragments.SharedViewModel
+import com.k3kc.todoapp20.fragments.list.adapter.ListAdapter
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(), SearchView.OnQueryTextListener {
 
     private val mToDoViewModel: ToDoViewModel by viewModels()
     private val mSharedViewModel: SharedViewModel by viewModels()
     private val adapter: ListAdapter by lazy { ListAdapter() }
 
-    private lateinit var _binding: FragmentListBinding
+    private var _binding: FragmentListBinding? = null
     private val binding
-        get() = _binding
-
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentListBinding.inflate(inflater, container, false)
-
-
-        val recyclerView = binding.recyclerView
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        binding.lifecycleOwner = this
+        binding.mSharedViewModel = mSharedViewModel
+        setupRecyclerView()
 
         mToDoViewModel.getAllData.observe(viewLifecycleOwner) { data ->
             mSharedViewModel.checkIfDatabaseEmpty(data)
             adapter.setData(data)
         }
 
-        mSharedViewModel.emptyDatabase.observe(viewLifecycleOwner) { isEmpty ->
-            showEmptyDatabaseViews(isEmpty)
-
-        }
-
         setHasOptionsMenu(true)
-
         return binding.root
-    }
-
-    private fun showEmptyDatabaseViews(emptyDatabase: Boolean) {
-        if(emptyDatabase) {
-            binding.noDataImageView.isVisible = true
-            binding.noDataTextView.isVisible = true
-        } else {
-            binding.noDataImageView.isVisible = false
-            binding.noDataTextView.isVisible = false
-        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -67,6 +56,43 @@ class ListFragment : Fragment() {
             confirmDeleteAll()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if(query != null) {
+            searchThroughDatabase(query)
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(query: String?): Boolean {
+        if(query != null) {
+            searchThroughDatabase(query)
+        }
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+        val search = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun searchThroughDatabase(query: String) {
+        var searchQuery = query
+        searchQuery = "%$searchQuery%"
+        mToDoViewModel.searchDatabase(searchQuery).observe(viewLifecycleOwner) {
+            it?.let {
+                adapter.setData(it )
+            }
+        }
     }
 
     private fun confirmDeleteAll() {
@@ -80,8 +106,44 @@ class ListFragment : Fragment() {
         confirmationDialog.create().show()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.list_fragment_menu, menu)
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+        val swipeToDeleteCallback = object : SwipeToDelete() {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val itemToDelete = adapter.dataList[viewHolder.adapterPosition]
+                mToDoViewModel.deleteItem(itemToDelete)
+                Toast
+                    .makeText(requireContext(), "Successfully deleted", Toast.LENGTH_SHORT)
+                    .show()
+                restoreDeletedData(requireView(), itemToDelete, viewHolder.adapterPosition)
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
+    private fun restoreDeletedData(view: View, deletedItem: ToDoData, position: Int) {
+        val snackbar = Snackbar.make(
+            view,
+            "Deleted '${deletedItem.title}'",
+            Snackbar.LENGTH_LONG
+        )
+        snackbar.setAction("Undo", object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                mToDoViewModel.insertData(deletedItem)
+                adapter.notifyItemChanged(position)
+            }
+        })
+        snackbar.show()
+    }
+
+    private fun setupRecyclerView() {
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(requireActivity(), 2)
+        recyclerView.itemAnimator = SlideInUpAnimator().apply {
+            addDuration = 300
+        }
+        swipeToDelete(recyclerView)
     }
 
 }
